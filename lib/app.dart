9ac0574c7c18a217d5dfea1f420c66cb15f4f44a
Biswaranjan Basak <biswaranjan.basak.contractor@pepsico.com>
2024-01-21 01:33:05 +0530

    Adding Assets

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'package:employee_app/presentation/core/app_assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'config.dart';
 import 'locator.dart';
@@ -35,10 +37,16 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Employee App $flavor'),
+          title: Text('Employee List $flavor'),
+          centerTitle: false,
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: SvgPicture.asset(
+            AppAssets.noEmployeeIcon,
+            height: 218,
+            width: 261,
+            fit: BoxFit.contain,
+          ),
         ),
       ),
     );
---
+++ lib/presentation/core/app_assets.dart
@@ -0,0 +1,4 @@
+abstract class AppAssets {
+  static const String _basePath = 'assets/svg/';
+  static const String noEmployeeIcon = '${_basePath}no_employee_icon.svg';
+}
